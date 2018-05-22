65057400598b5864fbe15271c0d66c1667ff503c
wright0776 <matwright2010@gmail.com>
2018-05-21 20:32:12 -0600

    created a cool loading screen with css animations

---
+++ css-trials/src/App/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.initialState = {
+
+        }
+        this.state = this.initialState;
+    }
+
+    render() {
+        return (
+            <div className='app'>
+                <div className='loading'>loading...</div>
+                <div className='fuzzy a'></div>
+                <div className='fuzzy b'></div>
+                <div className='fuzzy c'></div>
+                <div className='fuzzy d'></div>
+                <div className='fuzzy e'></div>
+                <div className='fuzzy f'></div>
+                <div className='fuzzy g'></div>
+                <div className='fuzzy h'></div>
+                <div className='fuzzy i'></div>
+                <div className='fuzzy j'></div>
+                <div className='fuzzy k'></div>
+                <div className='fuzzy l'></div>
+                <div className='fuzzy m'></div>
+                <div className='fuzzy n'></div>
+                <div className='fuzzy o'></div>
+                <div className='fuzzy p'></div>
+                <div className='fuzzy q'></div>
+                <div className='fuzzy r'></div>
+                <div className='fuzzy s'></div>
+                <div className='fuzzy t'></div>
+                <div className='fuzzy u'></div>
+            </div>
+        )
+    }
+}
+
+
