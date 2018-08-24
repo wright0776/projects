99a436d2c392ab5db769d384d76ea9590770a79c
wright0776 <matwright2010@gmail.com>
2018-08-24 12:32:58 -0600

    progress made on angular course

---
+++ python-course/dictionaries.py
@@ -0,0 +1,15 @@
+phonebook = {}
+phonebook["John"] = 8015558888
+phonebook["Jack"] = 8015559999
+phonebook["Jill"] = 8015551111
+print(phonebook)
+
+phonebook = {
+    "John" : 8015558888,
+    "Jack" : 8015559999,
+    "Jill" : 8015551111
+}
+print(phonebook)
+
+# Iterating over dictionaries
+
